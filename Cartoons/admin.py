fae69c8b8c21d9ef68866a5153dbdbed3ebda994
Max <logist.sklad.marvel@gmail.com>
2022-11-10 23:02:30 +0300

    ДБ поднастроена

--- Cartoons/admin.py
+++ Cartoons/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
-
+from . import models
 # Register your models here.
+
+
+class PrepopulatedURL(admin.ModelAdmin):
+    prepopulated_fields = {'url': ('name',)}
+
+
+admin.site.register(models.Cartoon, PrepopulatedURL)
+admin.site.register(models.CartoonCategory, PrepopulatedURL)
+admin.site.register(models.Episode, PrepopulatedURL)
+admin.site.register(models.Screenshot, PrepopulatedURL)
+admin.site.register(models.Actor, PrepopulatedURL)
+
+
--- Cartoons/models.py
+++ Cartoons/models.py
@@ -4,33 +4,35 @@ from . import utils
 # Create your models here.
 
 
+class Actor(utils.CommonFields):
+    photo = models.ImageField(upload_to='actors/', blank=True)
+
+
+class Screenshot(utils.CommonFields):
+    image = models.ImageField(upload_to='screenshots/')
+
+
+class Episode(utils.CommonFields):
+    episode_number = models.PositiveSmallIntegerField()
+    season_number = models.PositiveSmallIntegerField()
+    episode_url = models.URLField()
+
+
+class CartoonCategory(utils.CommonFields):
+    pass
+
+
 class Cartoon(utils.CommonFields):
     tagline = models.CharField('слоган мультика', max_length=100)
     year = models.PositiveSmallIntegerField('год выпуска')
     number_of_episodes = models.PositiveSmallIntegerField('количество серий')
     poster = models.ImageField(upload_to='posters/')
     country = models.CharField('Страна производитель', max_length=50)
+    actors = models.ManyToManyField(Actor)
+    screenshots = models.ForeignKey(Screenshot, on_delete=models.CASCADE)
+    episodes = models.ForeignKey(Episode, on_delete=models.CASCADE)
+    category = models.ForeignKey(CartoonCategory, on_delete=models.CASCADE)
 
-
-class Actor(utils.CommonFields):
-    photo = models.ImageField(upload_to='actors/', blank=True)
-    cartoon_actor = models.ManyToManyField(Cartoon)
-
-
-class Screenshot(utils.CommonFields):
-    image = models.ImageField(upload_to='screenshots/')
-    cartoon_sc = models.ForeignKey(Cartoon, on_delete=models.CASCADE)
-
-
-class Episode(utils.CommonFields):
-    episode_number = models.PositiveSmallIntegerField()
-    season_number = models.PositiveSmallIntegerField()
-    cartoon_ep = models.ForeignKey(Cartoon, on_delete=models.CASCADE)
-    episode_url = models.URLField()
-
-
-class CartoonCategory(utils.CommonFields):
-    cartoon_cat = models.ForeignKey(Cartoon, on_delete=models.CASCADE)
 
 
 
